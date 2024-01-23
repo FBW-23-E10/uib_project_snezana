a7a23145d07de3f5795261c75ba755134cf6c3d8
Snezana1412 <snezana.lalatovic@dci-student.org>
2024-01-23 08:40:49 +0100

    Added hero and about me sections

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".nav-link").on("click", function(){
+        $(".navbar").find(".active").removeClass("active");
+        $(this).addClass("active");
+     });
+
+
+ });
+
+
+(function() {
+    'use strict';
+
+   
+
+
+})();
