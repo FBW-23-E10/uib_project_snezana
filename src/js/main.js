d80ed85aea25bc29db0e2c012f05e86ae06168ad
Snezana1412 <snezana.lalatovic@dci-student.org>
2024-01-24 15:16:06 +0100

    Added Resume and Projects sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,85 @@
-$(document).ready(function(){
-    $(".nav-link").on("click", function(){
-        $(".navbar").find(".active").removeClass("active");
-        $(this).addClass("active");
-     });
+$(document).ready(function () {
+  $(".nav-link").on("click", function () {
+    $(".navbar")
+      .find(".active")
+      .removeClass("active");
+    $(this).addClass("active");
+  });
+});
 
+$(document).ready(function () {
+  $(".navi-li").on("click", function () {
+    $("#navi")
+      .find(".current")
+      .removeClass("current");
+    $(this).addClass("current");
+  });
+});
 
- });
+(function () {
+  "use strict";
 
+  /**
+   * Circular Progress Bar
+   */
 
-(function() {
-    'use strict';
+  const circularProgress =
+    document.querySelectorAll(
+      ".circular-progress"
+    );
 
-   
+  Array.from(circularProgress).forEach(
+    (progressBar) => {
+      const progressValue =
+        progressBar.querySelector(".percentage");
+      const innerCircle =
+        progressBar.querySelector(
+          ".inner-circle"
+        );
+      let startValue = 0,
+        endValue = Number(
+          progressBar.getAttribute(
+            "data-percentage"
+          )
+        ),
+        speed = 50,
+        progressColor = progressBar.getAttribute(
+          "data-progress-color"
+        );
+
+      const progress = setInterval(() => {
+        startValue++;
+        progressValue.innerHTML = startValue + '<sup class="small">%</sup>';
+        //progressValue.style.color = `${progressColor}`;
+        //progressValue.style.color = 'red';
+
+        innerCircle.style.backgroundColor = `${progressBar.getAttribute(
+          "data-inner-circle-color"
+        )}`;
+       
+        progressBar.style.background = `conic-gradient(${progressColor} ${
+          startValue * 3.6
+        }deg,${progressBar.getAttribute(
+          "data-bg-color"
+        )} 0deg)`;
+        if (startValue === endValue) {
+          clearInterval(progress);
+        }
+      
+      }, speed);
+     
+    }
+  );
+
+    /** 
+     * Carousel
+     */
+  var myCarousel = document.querySelector('#myCarousel')
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000,
+    wrap: false
+  })
+
 
 
 })();
